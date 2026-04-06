5d438dad03ac3456c24082b4a22bc608fb37345a
Yumin <minny6309@gmail.com>
2026-04-06 16:20:37 +0900

    imu exercise

--- imu/imu_direction.py
+++ imu/imu_direction.py
@@ -3,6 +3,7 @@ import rospy
 import time
 
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import direction
 from tf.transformations import euler_from_quaternion
 
 Imu_msg = None
@@ -10,15 +11,27 @@ Imu_msg = None
 def imu_callback(data):
   global Imu_msg
   Imu_msg = [data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w]
+  (roll, pitch, yaw) = euler_from_quaternion(Imu_msg)
+
+  data_direction = direction()
+
+  if yaw > 0.5:
+    data_direction.direction = "right"
+  elif yaw < -0.5:
+    data_direction.direction = "left"
+  else:
+    data_direction.direction = "forward"
+
+  pub.publish(data_direction)
 
 rospy.init_node("Imu_Print")
 rospy.Subscriber("imu", Imu, imu_callback)
+pub = rospy.Publisher("direction", direction, queue_size=10)
 
 while not rospy.is_shutdown():
   if Imu_msg == None:
     continue
 
-  (roll, pitch, yaw) = euler_from_quaternion(Imu_msg)
-  print('Roll:%.4f, Pitch:%.4f, Yaw:%.4f'%(roll, pitch, yaw))
+  
 
   time.sleep(1.0)
