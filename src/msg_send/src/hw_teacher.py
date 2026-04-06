3d11729abe369ac6bdaf07a85d41b390fc578100
조이현 <ybcho@xytron.co.kr>
2026-04-06 15:32:25 +0900

    [UPDATE]통신실습

--- src/msg_send/src/hw_teacher.py
+++ src/msg_send/src/hw_teacher.py
@@ -9,10 +9,14 @@ from msg_send.msg import from_xycar
 def callback(msg):
     reply = from_xycar()
     reply.data = "Good afternoon, " + msg.last_name + " " + msg.first_name
-    print(reply.data)
+
+    print("1. Name : ", msg.last_name + msg.first_name)
+    print("2. ID : ", msg.id_number)
+    print("3. Phone Number : ", msg.phone_number)
+    
     pub.publish(reply)
 
 rospy.init_node('teacher')
-pub = rospy.Publisher('msg_from_xycar', from_xycar, queue_size=10)
 sub = rospy.Subscriber('msg_to_xycar', to_xycar, callback)
+pub = rospy.Publisher('msg_from_xycar', from_xycar, queue_size=10)
 rospy.spin()
