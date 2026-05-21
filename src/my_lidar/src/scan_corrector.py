284b1734bc623b4ad9437c7140b5a7943e20d886
mango1228 <glacks1228@gmail.com>
2026-05-21 20:51:36 +0900

    라이다 로그 추가5

--- src/my_lidar/src/scan_corrector.py
+++ src/my_lidar/src/scan_corrector.py
@@ -2,24 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # scan_corrector.py - /scan 각도 오프셋 보정 후 /scan_corrected 재발행
-# rqt_reconfigure 슬라이더로 angle_offset_deg 실시간 조정 가능
+# angle_min/angle_max 에 오프셋을 더하는 것만으로 전체 스캔이 회전됨
 
 import math
 import rospy
 from sensor_msgs.msg import LaserScan
-from dynamic_reconfigure.server import Server
-from my_lidar.cfg import ScanCorrectorConfig
 
-offset_rad = 0.0
 pub = None
 
-def reconfigure_callback(config, level):
-    global offset_rad
-    offset_rad = math.radians(config.angle_offset_deg)
-    rospy.loginfo("[scan_corrector] angle_offset_deg = %.1f" % config.angle_offset_deg)
-    return config
-
 def scan_callback(msg):
+    offset_rad = math.radians(rospy.get_param('~angle_offset_deg', 0.0))
+
     corrected = LaserScan()
     corrected.header          = msg.header
     corrected.angle_min       = msg.angle_min       + offset_rad
@@ -37,7 +30,6 @@ def main():
     global pub
     rospy.init_node('scan_corrector')
     pub = rospy.Publisher('/scan_corrected', LaserScan, queue_size=1)
-    Server(ScanCorrectorConfig, reconfigure_callback)
     rospy.Subscriber('/scan', LaserScan, scan_callback, queue_size=1)
     rospy.spin()
 
