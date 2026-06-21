cd3e01697a86b0e663cdce9730151e1988eeb373
Yumin <minny6309@gmail.com>
2026-06-21 11:36:49 +0900

    "~ar_center_gain_scale", 2.0

--- src/my_motor/src/config.py
+++ src/my_motor/src/config.py
@@ -82,7 +82,7 @@ class Config:
         self.ar_rightmost_sec = rospy.get_param("~ar_rightmost_sec", 0.0)  # 오른쪽 부채꼴 추종(제거)
         self.ar_stop3_sec     = rospy.get_param("~ar_stop3_sec", 0.0)      # 3차 정지(오른쪽 제거로 0)
         self.ar_center_sec    = rospy.get_param("~ar_center_sec", 9.5)     # 중앙 최근접 부채꼴 추종
-        self.ar_center_gain_scale = rospy.get_param("~ar_center_gain_scale", 1.95)  # 중앙 추종 게인 배율(장애물 회피 강화)
+        self.ar_center_gain_scale = rospy.get_param("~ar_center_gain_scale", 2.0)  # 중앙 추종 게인 배율(장애물 회피 강화)
         self.ar_stop4_sec = rospy.get_param("~ar_stop4_sec", 0.0)  # 중앙 추종 끝난 후 4차 정지(제거)
         self.ar_stop5_sec = rospy.get_param("~ar_stop5_sec", 0.0)  # 라이다축소 구간 끝난 후 5차 정지(제거)
         # 왼쪽 부채꼴 후보 최소 각폭(도). 이 이상 벌어진 부채꼴 중 가장 왼쪽 선택
@@ -106,7 +106,7 @@ class Config:
         # AR 인식 후 이 시간(초) 동안 카메라 추종점 미사용, 라이다 추종점만 사용
         self.ar_lidar_only_sec = rospy.get_param("~ar_lidar_only_sec", 10.0)
         # 부채꼴 추종 구간(왼쪽/오른쪽/중앙) 전체에 적용되는 추종 계수(lidar_center_gain) 배율
-        self.ar_leftmost_gain_scale = rospy.get_param("~ar_leftmost_gain_scale", 2.1)
+        self.ar_leftmost_gain_scale = rospy.get_param("~ar_leftmost_gain_scale", 2.0)
 
         # 횡단보도 감지
         self.cross_white_thresh   = rospy.get_param("~cross_white_thresh", 180)
